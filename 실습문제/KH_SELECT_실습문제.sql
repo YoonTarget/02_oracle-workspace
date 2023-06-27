85154bb05fba06f1c3832b6b2dd6d94b48c176fe
YoonTarget <igeek437@gmail.com>
2023-06-27 21:54:02 +0900

    6/27

--- 실습문제/KH_SELECT_실습문제.sql
+++ 실습문제/KH_SELECT_실습문제.sql
@@ -20,7 +20,7 @@ SELECT HIRE_DATE, EMP_NAME, SALARY
 FROM EMPLOYEE;
 
 -- 6. EMPLOYEE테이블에서 이름, 연봉, 총수령액(보너스포함), 실수령액(총수령액 - (연봉*세금 3%)) 조회
-SELECT EMP_NAME, (SALARY * 12) AS "연봉", (SALARY + SALARY * BONUS) * 12 AS "총수령액(보너스 포함)", (SALARY + SALARY * BONUS) * 12 - (SALARY * 12 * 0.03) AS "실수령액"
+SELECT EMP_NAME, (SALARY * 12) AS "연봉", (SALARY + SALARY * NVL(BONUS, 0)) * 12 AS "총수령액(보너스 포함)", (SALARY + SALARY * NVL(BONUS, 0)) * 12 - (SALARY * 12 * 0.03) AS "실수령액"
 FROM EMPLOYEE;
 
 -- 7. EMPLOYEE테이블에서 SAL_LEVEL이 S1인 사원의 이름, 월급, 고용일, 연락처 조회
@@ -29,9 +29,9 @@ FROM EMPLOYEE
 WHERE SAL_LEVEL = 'S1';
 
 -- 8. EMPLOYEE테이블에서 실수령액(6번 참고)이 5천만원 이상인 사원의 이름, 월급, 실수령액, 고용일 조회
-SELECT EMP_NAME, SALARY, (SALARY + SALARY * BONUS) * 12 - (SALARY * 12 * 0.03) AS "실수령액", HIRE_DATE
+SELECT EMP_NAME, SALARY, (SALARY + SALARY * NVL(BONUS, 0)) * 12 - (SALARY * 12 * 0.03) AS "실수령액", HIRE_DATE
 FROM EMPLOYEE
-WHERE ((SALARY + SALARY * BONUS) * 12 - (SALARY * 12 * 0.03)) >= 50000000;
+WHERE ((SALARY + SALARY * NVL(BONUS, 0)) * 12 - (SALARY * 12 * 0.03)) >= 50000000;
 
 -- 9. EMPLOYEE테이블에 월급이 4000000이상이고 JOB_CODE가 J2인 사원의 전체 내용 조회
 SELECT *
@@ -68,14 +68,61 @@ SELECT EMP_NAME, SUBSTR (EMP_NO, 1, 2) || '년' AS "생년", SUBSTR (EMP_NO, 3, 2) 
 FROM EMPLOYEE;
 
 -- 16. EMPLOYEE테이블에서 사원명, 주민번호 조회 (단, 주민번호는 생년월일만 보이게 하고, '-'다음 값은 '*'로 바꾸기)
-SELECT EMP_NAME, EMP_NO
+SELECT EMP_NAME, RPAD (SUBSTR (EMP_NO, 1, 7), 14, '*') AS "주민번호"
+FROM EMPLOYEE;
 
 -- 17. EMPLOYEE테이블에서 사원명, 입사일-오늘, 오늘-입사일 조회 (단, 각 별칭은 근무일수1, 근무일수2가 되도록 하고 모두 정수(내림), 양수가 되도록 처리)
+SELECT EMP_NAME, ABS (FLOOR ( HIRE_DATE - SYSDATE)) AS "근무일수 1", FLOOR (SYSDATE - HIRE_DATE) AS "근무일수 2"
+FROM EMPLOYEE;
+
 -- 18. EMPLOYEE테이블에서 사번이 홀수인 직원들의 정보 모두 조회
+SELECT *
+FROM EMPLOYEE
+WHERE MOD (EMP_ID, 2) != 0;
+
 -- 19. EMPLOYEE테이블에서 근무 년수가 20년 이상인 직원 정보 조회
+SELECT *
+FROM EMPLOYEE
+WHERE EXTRACT (YEAR FROM SYSDATE) - EXTRACT (YEAR FROM HIRE_DATE) >= 20;
+
 --20. EMPLOYEE 테이블에서 사원명, 급여 조회 (단, 급여는 '\9,000,000' 형식으로 표시)
+SELECT EMP_NAME, TO_CHAR (SALARY, 'L9,999,999') || '원' AS "급여"
+FROM EMPLOYEE;
+
 -- 21. EMPLOYEE테이블에서 직원 명, 부서코드, 생년월일, 나이(만) 조회 (단, 생년월일은 주민번호에서 추출해서 00년 00월 00일로 출력되게 하며 나이는 주민번호에서 출력해서 날짜데이터로 변환한 다음 계산)
+SELECT EMP_NAME, DEPT_CODE, TO_CHAR (TO_DATE (SUBSTR (EMP_NO, 1, 6)), 'YY"년" MM"월" DD"일"') AS "생년월일", FLOOR ((SYSDATE - TO_DATE (SUBSTR (EMP_NO, 1, 6))) / 365) || '세' AS "나이(만)"
+FROM EMPLOYEE;
+
 -- 22. EMPLOYEE테이블에서 부서코드가 D5, D6, D9인 사원만 조회하되 D5면 총무부, D6면 기획부, D9면 영업부로 처리 (단, 부서코드 오름차순으로 정렬)
+SELECT EMP_ID, EMP_NAME, EMP_NO, EMAIL, PHONE, JOB_CODE, SAL_LEVEL, SALARY, BONUS, MANAGER_ID, HIRE_DATE, ENT_DATE, ENT_YN, DEPT_CODE,
+                CASE WHEN DEPT_CODE = 'D5' THEN '총무부'
+                          WHEN DEPT_CODE = 'D6' THEN '기획부'
+                          WHEN DEPT_CODE = 'D9' THEN '영업부'
+END AS "부서코드"
+FROM EMPLOYEE
+WHERE DEPT_CODE IN ('D5', 'D6', 'D9')
+ORDER BY DEPT_CODE;
+
 -- 23. EMPLOYEE테이블에서 사번이 201번인 사원명, 주민번호 앞자리, 주민번호 뒷자리, 주민번호 앞자리와 뒷자리의 합 조회
+SELECT EMP_NAME, SUBSTR (EMP_NO, 1, 6) AS "주민번호 앞자리", SUBSTR (EMP_NO, 8, 14) AS "주민번호 뒷자리", SUBSTR (EMP_NO, 1, 6) + SUBSTR (EMP_NO, 8, 14) AS "앞자리와 뒷자리의 합"
+FROM EMPLOYEE
+WHERE EMP_ID = 201;
+
 -- 24. EMPLOYEE테이블에서 부서코드가 D5인 직원의 보너스 포함 연봉 합 조회
+SELECT SUM (SALARY + (SALARY * NVL(BONUS, 0)) * 12) || '원' AS "연봉 합"
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5';
+
 -- 25. EMPLOYEE테이블에서 직원들의 입사일로부터 년도만 가지고 각 년도별 입사 인원수 조회 전체 직원 수, 2001년, 2002년, 2003년, 2004년
+SELECT COUNT(*) AS "전체 직원수"
+FROM EMPLOYEE
+WHERE EMP_ID IS NOT NULL;
+
+SELECT COUNT(*), COUNT(TO_CHAR(HIRE_DATE))
+FROM EMPLOYEE
+WHERE EXTRACT(YEAR FROM HIRE_DATE) BETWEEN 1990 AND 2017;
+
+SELECT *
+FROM EMPLOYEE
+ORDER BY HIRE_DATE;
+
---
+++ 실습문제/춘대학_실습문제.sql
@@ -0,0 +1,159 @@
+-- [Basic SELECT]
+
+-- 1. 춘 기술대학교의 학과 이름과 계열을 표시하시오. 단, 출력 헤더는 "학과 명", "계열"으로 표시하도록 한다.
+SELECT DEPARTMENT_NAME AS "학과 명", CATEGORY AS "계열"
+FROM TB_DEPARTMENT;
+
+-- 2. 학과의 학과 정원을 다음과 같은 형태로 화면에 출력한다.
+SELECT DEPARTMENT_NAME || '의 정원은 ' || CAPACITY || ' 명 입니다.' AS "학과별 정원"
+FROM TB_DEPARTMENT;
+
+-- 3. "국어국문학과"에 다니는 여학생 중 현재 휴학중인 여학생을 찾아달라는 요청이 들어왔다. 
+-- 누구인가? (국문학과의 '학과코드'는 학과 테이블(TB_DEPARTMENT)를 조회해서 찾아 내도록 하자)
+SELECT STUDENT_NAME
+FROM TB_STUDENT
+WHERE DEPARTMENT_NO = 001 AND ABSENCE_YN = 'Y' AND SUBSTR (STUDENT_SSN, 8, 1) IN (2, 4);
+
+-- 4. 도서관에서 대출 도서 장기 연체자들을 찾아 이름을 게시하고자 한다. 그 대상자들의 학번이 다음과 같은 때 대상자들을 찾는 적절한 SQL 구문을 작성하시오.
+-- A513079, A513090, A513091, A513110, A513119
+SELECT *
+FROM TB_STUDENT
+WHERE STUDENT_NAME LIKE '%경희' OR STUDENT_NAME LIKE '%경환' OR STUDENT_NAME LIKE '정경%';
+
+-- 5. 입학정원이 20명 이상 30명 이하인 학과들의 학과 이름과 계열을 출력하시오.
+SELECT DEPARTMENT_NAME, CATEGORY
+FROM TB_DEPARTMENT
+WHERE CAPACITY BETWEEN 20 AND 30;
+
+-- 6. 춘 기술대학교는 총장을 제외하고 모든 교수들이 소속 학과를 가지고 있다. 그럼 춘 기술대학교 총장의 이름을 알아낼 수 있는 SQL 문장을 작성하시오.
+SELECT PROFESSOR_NAME
+FROM TB_PROFESSOR
+WHERE DEPARTMENT_NO IS NULL;
+
+-- 7. 혹시 전산상의 착오로 학과가 지정되어 있지 않은 학생이 있는지 확인하고자 한다.
+-- 어떠한 SQL 문장을 사용하면 될 것인지 작성하시오.
+SELECT *
+FROM TB_STUDENT
+WHERE DEPARTMENT_NO IS NULL;
+
+-- 8. 수강신청을 하려고 한다. 선수과목 여부를 확인해야 하는데,
+-- 선수과목이 존재하는 과목들은 어떤 과목인지 과목번호를 조회해보시오.
+SELECT CLASS_NO
+FROM TB_CLASS
+WHERE PREATTENDING_CLASS_NO IS NOT NULL;
+
+-- 9. 춘 대학에는 어떤 계열들이 있는지 조회해보시오.
+SELECT DISTINCT CATEGORY
+FROM TB_DEPARTMENT;
+
+-- 10. 02학번 전주 거주자들의 모임을 만들려고 한다. 휴학한 사람들은 제외한
+-- 재학중인 학생들의 학번, 이름, 주민번호를 출력하는 구문을 작성하시오.
+SELECT STUDENT_NO, STUDENT_NAME, STUDENT_SSN
+FROM TB_STUDENT
+WHERE STUDENT_NO LIKE 'A2%' AND ABSENCE_YN = 'N' AND STUDENT_ADDRESS LIKE '전주시%'
+ORDER BY STUDENT_NAME;
+
+-- [Additional SELECT - 함수]
+
+-- 1. 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학년도를 입학년도가 빠른 순으로 표시하는 SQL 문장을 작성하시오.
+-- (단, 헤더는 "학번", "이름", "입학년도"가 표시되도록 한다.)
+SELECT STUDENT_NO AS "학번", STUDENT_NAME AS "이름", TO_CHAR(ENTRANCE_DATE, 'YYYY-MM-DD') AS "입학년도"
+FROM TB_STUDENT
+WHERE DEPARTMENT_NO = 002
+ORDER BY ENTRANCE_DATE;
+
+-- 2. 춘 기술대학교의 교수 중 이름이 세 글자가 아닌 교수가 한 명 있다고 한다.
+-- 그 교수의 이름과 주민번호를 화면에 출력하는 SQL 문장을 작성해보자.
+-- (* 이때 올바르게 작성한 SQL 문장의 결과값이 예상과 다르게 나올 수 있다.
+-- 원인이 무엇일지 생각해볼 것)
+SELECT PROFESSOR_NAME, PROFESSOR_SSN
+FROM TB_PROFESSOR
+WHERE LENGTH(PROFESSOR_NAME) != 3;
+-- 교수 이름의 길이가 3이 아닌 것을 출력하는 것이기 때문에 2글자와 4글자의 이름이 모두 출력된다.
+
+-- 3.춘 기술대학교의 남자 교수들의 이름과 나이를 출력하는 SQL 문장을 작성하시오.
+-- 단, 이때 나이가 적은 사람에서 많은 사람 순서로 화면에 출력되도록 만드시오.
+-- (단, 교수 중 2000년 이후 출생자는 없으며 출력 헤더는 "교수이름", "나이"로 한다.
+-- 나이는 '만'으로 계산한다.)
+SELECT PROFESSOR_NAME AS "교수이름", FLOOR((SYSDATE - TO_DATE(SUBSTR(PROFESSOR_SSN, 1, 6) + 19000000)) / 365) - 1 AS "나이(만)"
+FROM TB_PROFESSOR
+WHERE SUBSTR(PROFESSOR_SSN, 8, 1) = 1
+ORDER BY PROFESSOR_SSN DESC, PROFESSOR_NAME;
+
+-- 4. 교수들의 이름 중 성을 제외한 이름만 출력하는 SQL 문장을 작성하시오.
+-- 출력 헤더는 "이름"이 찍히도록 한다. (성이 2자인 경우는 교수는 없다고 가정하시오.)
+SELECT SUBSTR(PROFESSOR_NAME, 2) AS "이름"
+FROM TB_PROFESSOR;
+
+-- 5. 춘 기술대학교의 재수생 입학자를 구하려고 한다. 어떻게 찾아낼 것인가?
+-- 이때, 19살에 입학하면 재수를 하지 않은 것으로 간주한다.
+SELECT STUDENT_NO, STUDENT_NAME
+FROM TB_STUDENT
+WHERE EXTRACT(YEAR FROM ENTRANCE_DATE)- EXTRACT(YEAR FROM TO_DATE(SUBSTR(STUDENT_SSN, 1, 6))) > 19;
+
+-- 6. 2020년 크리스마스는 무슨 요일인가?
+SELECT TO_CHAR(TO_DATE(201225), 'DAY') AS "2020년 크리스마스" FROM DUAL;
+
+-- 7. TO_DATE('99/10/11', 'YY/MM/DD'), TO_DATE('49/10/11', 'YY/MM/DD')은 각각 몇년 몇월 며칠을 의미할까?
+-- 또 TO_DATE('99/10/11', 'RR/MM/DD'), TO_DATE('49/10/11', 'RR/MM/DD')은 각각 몇년 몇월 며칠을 의미할까?
+SELECT TO_DATE('99/10/11', 'YY/MM/DD'), TO_DATE('49/10/11', 'YY/MM/DD'), TO_DATE('99/10/11', 'RR/MM/DD'), TO_DATE('49/10/11', 'RR/MM/DD')
+FROM DUAL;
+
+-- 8. 춘 기술대학교의 2000년도 이후 입학자들은 학번이 A로 시작하게 되어있다.
+-- 2000년도 이전 학번을 받은 학생들의 학번과 이름을 보여주는 SQL 문장을 작성하시오.
+SELECT *
+FROM TB_STUDENT
+WHERE STUDENT_NO NOT LIKE 'A%'
+ORDER BY STUDENT_NAME;
+
+-- 9. 학번이 A517178인 한아름 학생의 학점 총 평점을 구하는 SQL 문을 작성하시오.
+-- 단, 이때 출력 화면의 헤더는 "평점"이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 한 자리까지만 표시한다.
+SELECT ROUND(AVG(POINT), 1) AS "평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A517178';
+
+-- 10. 학과별 학생수를 구하여 "학과번호", "학생수(명)"의 형태로 헤더를 만들어 결과값이 출력되도록 하시오.
+SELECT DEPARTMENT_NO AS "학과번호", COUNT(*) AS "학생수(명)"
+FROM TB_STUDENT
+GROUP BY DEPARTMENT_NO
+ORDER BY DEPARTMENT_NO;
+
+-- 11. 지도교수를 배정받지 못한 학생의 수는 몇 명 정도 되는지 알아내는 SQL 문을 작성하시오.
+SELECT COUNT(*)
+FROM TB_STUDENT
+WHERE COACH_PROFESSOR_NO IS NULL;
+
+-- 12. 학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL문을 작성하시오.
+-- 단, 이때 출력 화면의 헤더는 "년도", "년도 별 평점"이라고 찍히게 하고,
+-- 점수는 반올림 하여 소수점 이하 한 자리까지만 표시한다.
+SELECT SUBSTR(TERM_NO, 1, 4) AS "년도", ROUND(AVG(POINT), 1) AS "년도 별 평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113'
+GROUP BY SUBSTR(TERM_NO, 1, 4)
+ORDER BY SUBSTR(TERM_NO, 1, 4);
+
+-- 13. 학과 별 휴학생 수를 파악하고자 한다. 학과 번호와 휴학생 수를 표시하는 SQL 문장을 작성하시오.
+SELECT DEPARTMENT_NO AS "학과코드명", COUNT(REPLACE(ABSENCE_YN, 'N', NULL))
+FROM TB_STUDENT
+GROUP BY DEPARTMENT_NO
+ORDER BY DEPARTMENT_NO;
+
+-- 14. 춘 대학교에 다니는 동명이인 학생들의 이름을 찾고자 한다.
+-- 어떤 SQL 문장을 사용하면 가능하겠는가?
+SELECT STUDENT_NAME, SUM(1)
+FROM TB_STUDENT
+GROUP BY STUDENT_NAME
+HAVING SUM(1) != 1
+ORDER BY STUDENT_NAME;
+
+-- 15. 학번이 A112113인 김고운 학생의 년도, 학기 별 평점과 년도 별 누적 평점, 총 평점을 구하는 SQL 문을 작성하시오.
+-- (단, 평점은 소수점 1자리까지만 반올림하여 표시한다.)
+SELECT SUBSTR(TERM_NO, 1, 4), SUBSTR(TERM_NO, 5), ROUND(AVG(POINT), 1)
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113'
+GROUP BY SUBSTR(TERM_NO, 1, 4), SUBSTR(TERM_NO, 5)
+ORDER BY SUBSTR(TERM_NO, 1, 4);
+
+SELECT *
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113';
